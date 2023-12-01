6a3a140092284c2641666fd54532881556b9fed2
adwabh <avabhyankar22@gmail.com>
2023-12-01 09:49:08 +0530

    [NOTE-001][Adwait] fixed issues with put endpoint

--- src/main/kotlin/com/arth/notee/controller/NotesConfig.kt
+++ src/main/kotlin/com/arth/notee/controller/NotesConfig.kt
@@ -5,17 +5,22 @@ import com.arth.notee.network.NoteResponse
 import com.arth.notee.network.NotesListResponse
 import com.arth.notee.network.Response
 import com.arth.notee.repository.NotesCoRoutinesRepository
-import com.arth.notee.repository.NotesEntity
+import com.arth.notee.repository.Notes
 import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.reactor.awaitSingle
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.http.HttpMethod
 import org.springframework.http.HttpStatus
+import org.springframework.web.ErrorResponse
 import org.springframework.web.reactive.function.server.*
 import java.lang.Exception
+import java.lang.IllegalArgumentException
+import java.sql.Timestamp
 import java.time.OffsetDateTime
+import java.time.ZoneOffset.UTC
+import java.util.TimeZone
+import java.util.UUID
 
 @Configuration
 class NotesConfig {
@@ -24,44 +29,11 @@ class NotesConfig {
     fun http(@Autowired repo: NotesCoRoutinesRepository): RouterFunction<ServerResponse> {
 
         return coRouter {
-            val updateOrSaveBlock: suspend (ServerRequest) -> ServerResponse = {
-                val predicate: (ServerRequest.Headers) -> Boolean =
-                    { it.header("title").isNotEmpty() || it.header("body").isNotEmpty() }
-                val entity = if (it.method() == HttpMethod.POST) {
-                    NotesEntity(
-                        it.pathVariable("id"),
-                        it.headers().header("title").first(),
-                        it.headers().header("body").first(),
-                        OffsetDateTime.now().toEpochSecond(),
-                        OffsetDateTime.now().toEpochSecond()
-                    )
-                } else if (it.method() == HttpMethod.PUT) {
-                    repo.findById(it.pathVariable("id"))
-                } else {
-                    null
-                }
-                val successResponse: suspend (ServerRequest) -> ServerResponse = {
-                    if (entity != null) {
-                        val id = repo.save(entity)
-                        ServerResponse.ok().bodyValueAndAwait(Response.success("", id))
-                    } else {
-                        ServerResponse.noContent().buildAndAwait()
-                    }
-                }
-                if(predicate(it.headers())) {
-                    successResponse(it)
-                } else {
-                    ServerResponse.badRequest().buildAndAwait()
-                }
-            }
-
             GET("/v1/notes") {
                 val destination = mutableListOf<NoteResponse>()
 
-                val transform: suspend (value: NotesEntity) -> NoteResponse = {
-                    NoteResponse(
-                        it.id, it.title, it.body, it.createdDate, it.updatedDate
-                    )
+                val transform: suspend (value: Notes) -> NoteResponse = {
+                    it.toResponse()!!
                 }
                 val notes = repo.findAll().map(transform).toList(destination)
                 ServerResponse.ok().bodyValue(Response.success("", NotesListResponse(notes))).awaitSingle()
@@ -81,9 +53,60 @@ class NotesConfig {
                 response
             }
 
-            POST("/v1/note/{id}", updateOrSaveBlock)
+            POST("/v1/note") {
+                with(it.headers()) {
+                    if (header("title").isNotEmpty() ||
+                        header("body").isNotEmpty()) {
+                        val note = Notes(
+                            null,
+                            it.headers().header("title").first(),
+                            it.headers().header("body").first(),
+                            OffsetDateTime.now(),
+                            OffsetDateTime.now()
+                            )
+                        val id = repo.save(note)
+                        ServerResponse.ok().bodyValue(Response.success("S200", id)).awaitSingle()
+                    } else {
+                        ServerResponse.from(
+                            ErrorResponse.builder(
+                                IllegalArgumentException(),
+                                HttpStatus.BAD_REQUEST,
+                                "empty value for inputs"
+                            ).build()
+                        ).awaitSingle()
+                    }
+                }
+            }
 
-            PUT("v1/note/{id}", updateOrSaveBlock)
+            PUT("v1/note/{id}") {
+                val entity = repo.findById(it.pathVariable("id"))
+                with(it.headers()){
+                    if (header("title").isNotEmpty() ||
+                        header("body").isNotEmpty()) {
+                        if (entity != null) {
+                            val update = entity.copy(title = header("title").first(), body = header("body").first())
+                            val id = repo.save(update)
+                            ServerResponse.ok().bodyValueAndAwait(Response.success("S200", id))
+                        } else {
+                            ServerResponse.from(
+                                ErrorResponse.builder(
+                                    IllegalArgumentException(),
+                                    HttpStatus.NO_CONTENT,
+                                    "Something went wrong"
+                                ).build()
+                            ).awaitSingle()
+                        }
+                    } else {
+                        ServerResponse.from(
+                            ErrorResponse.builder(
+                                IllegalArgumentException(),
+                                HttpStatus.BAD_REQUEST,
+                                "empty value for inputs"
+                            ).build()
+                        ).awaitSingle()
+                    }
+                }
+            }
 
             DELETE("/v1/note/{id}") {
                 val id = repo.findById(it.pathVariable("id"))
@@ -98,10 +121,10 @@ class NotesConfig {
     }
 }
 
-private fun NotesEntity?.toResponse(): NoteResponse? {
+fun Notes?.toResponse(): NoteResponse? {
     return this?.let {
         NoteResponse(
-            it.id, it.title, it.body, it.createdDate, it.updatedDate
+            it.id?.toString()?:"", it.title, it.body, it.createdDate.toEpochSecond(), it.updatedDate.toEpochSecond()
         )
     }
 }
--- src/main/kotlin/com/arth/notee/repository/NotesCoRoutinesRepository.kt
+++ src/main/kotlin/com/arth/notee/repository/NotesCoRoutinesRepository.kt
@@ -1,13 +1,18 @@
 package com.arth.notee.repository
 
 import org.springframework.data.annotation.Id
+import org.springframework.data.domain.Persistable
 import org.springframework.data.repository.kotlin.CoroutineCrudRepository
+import java.sql.Timestamp
+import java.time.OffsetDateTime
 
-interface NotesCoRoutinesRepository : CoroutineCrudRepository<NotesEntity, String>
-data class NotesEntity(
-    @Id val id: String,
+interface NotesCoRoutinesRepository : CoroutineCrudRepository<Notes, String>
+
+data class Notes(
+    @Id val id: Int? = null,
     val title: String,
     val body: String,
-    val createdDate: Long,
-    val updatedDate: Long
+    val createdDate: OffsetDateTime,
+    val updatedDate: OffsetDateTime,
 )
+
